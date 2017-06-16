15aa452b3674cb1f077f3f25586e253d66d92d63
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-06-17 00:58:41 +0200

    [tx-robot] updated from transifex

--- l10n/pl.js
+++ l10n/pl.js
@@ -40,4 +40,4 @@ OC.L10N.register(
     "Removed from group \"%2$s\"." : "Usunięto z grupy \"%2$s\".",
     "You have been removed from the group \"%2$s\" by \"%1$s\"." : "Zostałeś usunięty z grupy \"%2$s\" przez \"%1$s\"."
 },
-"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>=14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
+"nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
