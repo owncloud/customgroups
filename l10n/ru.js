b83d1fa50402a50463d792603f06d647dce74036
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-06-24 00:58:40 +0200

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -42,6 +42,7 @@ OC.L10N.register(
     "You have been removed from the group \"%2$s\" by \"%1$s\"." : "\"%1$s\" удалил вас из группы \"%2$s\".",
     "Role change in group \"%1$s\"." : "Изменение роли в группе \"%1$s\".",
     "\"%1$s\" assigned the \"%3$s\" role for the group \"%2$s\" to you." : "\"%1$s\" присвоил вам роль \"%3$s\" в группе \"%2$s\".",
-    "Unknown role" : "Неизвестная роль"
+    "Unknown role" : "Неизвестная роль",
+    "Only group admins are allowed to create custom groups" : "Только администраторам групп разрешено создавать пользовательские группы"
 },
 "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
