a2028f1bee7803dda0fb0d4664b6eb6427e2a311
Felix Heidecke <felixheidecke@users.noreply.github.com>
2017-09-11 16:42:22 +0200

    use compliant operator

--- appinfo/app.php
+++ appinfo/app.php
@@ -25,7 +25,7 @@ $app->registerNotifier();
 
 if(!defined('PHPUNIT') && !\OC::$CLI) {
    $pathInfo = \OC::$server->getRequest()->getPathInfo();
-   if (strstr($pathInfo, 'settings/') != false) {
+   if (strstr($pathInfo, 'settings/') !== false) {
       // Temporarily fix icon until custom icons are supported
       \OCP\Util::addStyle('customgroups', 'icon');
    }
