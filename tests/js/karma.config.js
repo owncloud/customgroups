7fd5f956af0cbf2a73b4480121655fd4bb56164d
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2023-04-13 15:33:50 +0200

    fix: use ChromeHeadless instead of PhantomJS

--- tests/js/karma.config.js
+++ tests/js/karma.config.js
@@ -105,7 +105,13 @@ module.exports = function(config) {
 		// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
 		// - PhantomJS
 		// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-		browsers: ['PhantomJS'],
+		browsers: ['ChromeHeadlessNoSandbox'],
+		customLaunchers: {
+			ChromeHeadlessNoSandbox: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+		},
 
 		// If browser does not capture in given timeout [ms], kill it
 		captureTimeout: 60000,
