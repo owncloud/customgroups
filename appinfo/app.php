3eaa0bc81e87f97fa9ec6610f9ee29d39b073593
Tom Wezepoel <tom.wezepoel@surf.nl>
2023-06-19 11:52:27 +0200

    Add Menu Icon

--- appinfo/app.php
+++ appinfo/app.php
@@ -30,3 +30,26 @@ if (!\defined('PHPUNIT') && !\OC::$CLI) {
 		\OCP\Util::addStyle('customgroups', 'icon');
 	}
 }
+
+\OC::$server->getNavigationManager()->add(function () {
+	$urlGenerator = \OC::$server->getURLGenerator();
+	return [
+		// the string under which your app will be referenced in owncloud
+		'id' => 'customgroups',
+
+		// sorting weight for the navigation. The higher the number, the higher
+		// will it be listed in the navigation
+		'order' => 90,
+
+		// the route that will be shown on startup
+		'href' => $urlGenerator->linkToRoute('settings.SettingsPage.getPersonal', ['sectionid' => 'customgroups']),
+
+		// the icon that will be shown in the navigation
+		// this file needs to exist in img/
+		'icon' => $urlGenerator->imagePath('customgroups', 'app.svg'),
+
+		// the title of your application. This will be used in the
+		// navigation or on the settings page of your app
+		'name' => \OC::$server->getL10N('customgroups')->t('Groups'),
+	];
+});
